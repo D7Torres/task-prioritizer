e69f9b802d808a0e190c7670d68591d52cd25703
David Torres <david7torres@gmail.com>
2024-11-20 16:30:11 +0100

    chore: replace css file with tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import './App.css'
-
 function App() {
   return (
     <>
-      <nav className="navbar">
+      <nav className="bg-neutral-800 p-4">
         <button>Add List</button>
       </nav>
     </>
